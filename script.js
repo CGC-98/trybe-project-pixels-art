e8de6b2fa8fbc859ebd47c325b9a6b34038e1c42
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-31 18:04:04 -0300

    triple storage

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const btnColor = document.getElementById('button-random-color');
 const colorSection = document.querySelector('#colorSection').children;
 
-console.log(colorSection[1]);
-
 // Generate Random Color from CSS-TRICKS
 function attribute() {
   for (let i = 1; i < colorSection.length; i += 1) {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     const colorOutput = `#${randomColor}`;
-    console.log(colorOutput);
     colorSection[i].style.backgroundColor = colorOutput;
+    const toStore = `colorPallete[${i}]`;
+    localStorage.setItem(toStore, colorOutput);
   }
 }
 
 btnColor.addEventListener('click', attribute);
+
+// localStorage;
+// window.onload = function () {
+//   if (typeof (Storage) !== 'undefined') {
+//     if (localStorage.colorPalette !== undefined) {
+//       colorSection[1].style.backgroundColor = localStorage.getItem('colorPalette');
+//     }
+//   } else {
+//     document.write('Sem suporte para Web Storage');
+//   }
+// };
