549bdb8fcc6d20a9f54852145473f99a3067fde1
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-31 18:56:22 -0300

    up to four

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 const btnColor = document.getElementById('button-random-color');
-const colorSection = document.querySelector('#colorSection').children;
+const colorSection = document.querySelector('#color-palette').children;
 
 // Generate Random Color from CSS-TRICKS
 function attribute() {
-  for (let i = 1; i < colorSection.length; i += 1) {
+  for (let i = 1; i < colorSection.length - 1; i += 1) {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     const colorOutput = `#${randomColor}`;
     colorSection[i].style.backgroundColor = colorOutput;
-    const toStore = `colorPallete[${i}]`;
-    localStorage.setItem(toStore, colorOutput);
+    const setPalette = `colorPalette${i}`;
+    localStorage.setItem(setPalette, colorOutput);
   }
 }
 
+function storageCheck() {
+  if (localStorage.colorPalette1) {
+    colorSection[1].style.backgroundColor = localStorage.colorPalette1;
+    colorSection[2].style.backgroundColor = localStorage.colorPalette2;
+    colorSection[3].style.backgroundColor = localStorage.colorPalette3;
+  }
+}
+
+window.onload = function onload() {
+  if (Storage) {
+    storageCheck();
+  } else {
+    document.write('Sem suporte para Web Storage');
+  }
+};
+
+// Listeners;
 btnColor.addEventListener('click', attribute);
 
-// localStorage;
-// window.onload = function () {
-//   if (typeof (Storage) !== 'undefined') {
-//     if (localStorage.colorPalette !== undefined) {
-//       colorSection[1].style.backgroundColor = localStorage.getItem('colorPalette');
-//     }
-//   } else {
-//     document.write('Sem suporte para Web Storage');
-//   }
-// };
+// function changeColor() {
+//   var userColor = document.getElementById('colorID').value;
+//   localStorage.setItem('storedValue', document.body.style.backgroundColor = userColor);
+// }
+
+// // if there is a value stored, update color picker and background color
+// if (localStorage.storedValue) {
+//   document.getElementById('colorID').value = localStorage.storedValue;
+//   document.body.style.backgroundColor = localStorage.storedValue;
+// }
