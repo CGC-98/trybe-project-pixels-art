0979c0c75c2383f41c5ac2abfed60098f78b163b
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-31 19:22:36 -0300

    quest 5 completion

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ const colorSection = document.querySelector('#color-palette').children;
 
 // Generate Random Color from CSS-TRICKS
 function attribute() {
+  const colorArray = [];
   for (let i = 1; i < colorSection.length - 1; i += 1) {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     const colorOutput = `#${randomColor}`;
     colorSection[i].style.backgroundColor = colorOutput;
-    const setPalette = `colorPalette${i}`;
-    localStorage.setItem(setPalette, colorOutput);
+    colorArray.push(colorOutput);
   }
+  localStorage.setItem('colorPalette', JSON.stringify(colorArray));
 }
 
 function storageCheck() {
-  if (localStorage.colorPalette1) {
-    colorSection[1].style.backgroundColor = localStorage.colorPalette1;
-    colorSection[2].style.backgroundColor = localStorage.colorPalette2;
-    colorSection[3].style.backgroundColor = localStorage.colorPalette3;
+  if (localStorage.colorPalette) {
+    const colorReturn = JSON.parse(localStorage.getItem('colorPalette'));
+    for (let i = 1; i <= colorReturn.length; i += 1) {
+      colorSection[i].style.backgroundColor = colorReturn[i - 1];
+    }
   }
 }
 
@@ -30,14 +32,3 @@ window.onload = function onload() {
 
 // Listeners;
 btnColor.addEventListener('click', attribute);
-
-// function changeColor() {
-//   var userColor = document.getElementById('colorID').value;
-//   localStorage.setItem('storedValue', document.body.style.backgroundColor = userColor);
-// }
-
-// // if there is a value stored, update color picker and background color
-// if (localStorage.storedValue) {
-//   document.getElementById('colorID').value = localStorage.storedValue;
-//   document.body.style.backgroundColor = localStorage.storedValue;
-// }
