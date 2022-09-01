24b6ad529a66f5af9b52727c30fff1fc55cad581
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-09-01 00:30:01 -0300

    2.2/8.1 conflict; window.onload?

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const btnColor = document.getElementById('button-random-color');
 const colorSection = document.getElementById('color-palette').children;
 const pixelSection = document.getElementsByClassName('pixel');
 const selected = document.getElementsByClassName('selected');
-let selectedColor = 'black';
 
 // Color Predefinition;
 colorSection[0].style.backgroundColor = 'black';
@@ -10,7 +9,18 @@ colorSection[1].style.backgroundColor = 'red';
 colorSection[2].style.backgroundColor = 'green';
 colorSection[3].style.backgroundColor = 'blue';
 
+let selectedColor = colorSection[0].style.backgroundColor;
+
 // Generate Random Color from CSS-TRICKS
+function storageOutput() {
+  if (localStorage.colorPalette) {
+    const colorReturn = JSON.parse(localStorage.getItem('colorPalette'));
+    for (let i = 1; i <= colorReturn.length; i += 1) {
+      colorSection[i].style.backgroundColor = colorReturn[i - 1];
+    }
+  }
+}
+
 function randomize() {
   const colorArray = [];
   for (let i = 1; i < colorSection.length - 1; i += 1) {
@@ -22,50 +32,36 @@ function randomize() {
   localStorage.setItem('colorPalette', JSON.stringify(colorArray));
 }
 
-function storageOutput() {
-  if (localStorage.colorPalette) {
-    const colorReturn = JSON.parse(localStorage.getItem('colorPalette'));
-    for (let i = 1; i <= colorReturn.length; i += 1) {
-      colorSection[i].style.backgroundColor = colorReturn[i - 1];
-    }
-  }
-}
-
-function defineSelected(event) {
+function colorSelect(event) {
   for (let i = 0; i < selected.length; i += 1) {
     selected[i].className = 'color';
   }
   const clickedColor = event.target;
   clickedColor.className = 'selected';
-  return clickedColor;
+  selectedColor = clickedColor.style.backgroundColor;
 }
 
-function colorSelect(event) {
-  selectedColor = defineSelected(event).style.backgroundColor;
-}
-
-function pixelColor(event) {
+function colorDrop(event) {
   const clickedPixel = event.target;
   clickedPixel.style.backgroundColor = selectedColor;
-  console.log(clickedPixel.style.backgroundColor);
 }
 
+// Listeners;
 window.onload = function onload() {
   if (Storage) {
     storageOutput();
   } else {
     document.write('Sem suporte para Web Storage');
   }
+  // colorSection[0].className = 'selected';
 };
 
-// PROGRESS;
-// Listeners;
 btnColor.addEventListener('click', randomize);
-
-for (let i = 0; i < pixelSection.length; i += 1) {
-  pixelSection[i].addEventListener('click', pixelColor);
-}
 
 for (let i = 0; i < colorSection.length - 1; i += 1) {
   colorSection[i].addEventListener('click', colorSelect);
+}
+
+for (let i = 0; i < pixelSection.length; i += 1) {
+  pixelSection[i].addEventListener('click', colorDrop);
 }
