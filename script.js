6145fc3119b32fb4d2e336985d2357fde041330b
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-31 23:59:27 -0300

    color discrimination

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const btnColor = document.getElementById('button-random-color');
-const colorSection = document.querySelector('#color-palette').children;
-const lineCounter = document.getElementsByClassName('boxLine');
-console.log(lineCounter);
+const colorSection = document.getElementById('color-palette').children;
+const pixelSection = document.getElementsByClassName('pixel');
+const selected = document.getElementsByClassName('selected');
+let selectedColor = 'black';
+
+// Color Predefinition;
+colorSection[0].style.backgroundColor = 'black';
+colorSection[1].style.backgroundColor = 'red';
+colorSection[2].style.backgroundColor = 'green';
+colorSection[3].style.backgroundColor = 'blue';
 
 // Generate Random Color from CSS-TRICKS
-function attribute() {
+function randomize() {
   const colorArray = [];
   for (let i = 1; i < colorSection.length - 1; i += 1) {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -15,7 +22,7 @@ function attribute() {
   localStorage.setItem('colorPalette', JSON.stringify(colorArray));
 }
 
-function storageCheck() {
+function storageOutput() {
   if (localStorage.colorPalette) {
     const colorReturn = JSON.parse(localStorage.getItem('colorPalette'));
     for (let i = 1; i <= colorReturn.length; i += 1) {
@@ -24,20 +31,41 @@ function storageCheck() {
   }
 }
 
+function defineSelected(event) {
+  for (let i = 0; i < selected.length; i += 1) {
+    selected[i].className = 'color';
+  }
+  const clickedColor = event.target;
+  clickedColor.className = 'selected';
+  return clickedColor;
+}
+
+function colorSelect(event) {
+  selectedColor = defineSelected(event).style.backgroundColor;
+}
+
+function pixelColor(event) {
+  const clickedPixel = event.target;
+  clickedPixel.style.backgroundColor = selectedColor;
+  console.log(clickedPixel.style.backgroundColor);
+}
+
 window.onload = function onload() {
   if (Storage) {
-    storageCheck();
+    storageOutput();
   } else {
     document.write('Sem suporte para Web Storage');
   }
 };
 
-// Listeners;
-btnColor.addEventListener('click', attribute);
-
 // PROGRESS;
-// for (let i = 0; i < lineCounter.length; i += 1) {
-//   for (let j = 0; i < 5; j += 1) {
-//     let perLine = lineCounter[i];
-//   }
-// }
+// Listeners;
+btnColor.addEventListener('click', randomize);
+
+for (let i = 0; i < pixelSection.length; i += 1) {
+  pixelSection[i].addEventListener('click', pixelColor);
+}
+
+for (let i = 0; i < colorSection.length - 1; i += 1) {
+  colorSection[i].addEventListener('click', colorSelect);
+}
